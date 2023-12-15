c949ee98a2f6b57aee87806b76d30c9b243b90dc
Nathan Cortez <nath.co03@icloud.com>
2023-12-15 00:49:41 -0500

    code clean up

--- src/components/main.ts
+++ src/components/main.ts
@@ -5,16 +5,15 @@ fetch("/src/assets/wordList.json")
   .then((data) => {
     // Use data as needed
     words = data;
-    console.log(data);
   })
   .catch((error) => console.error("Error loading JSON:", error));
 
 const wordOfTheDay = "gamer";
-const wordList = ["gamer", "audio", "yoink"];
 let tries = 0;
 let guess = 0;
 let row = 1;
 const letter = /^[a-zA-Z]+$/;
+
 document.addEventListener("keyup", (event) => {
   const wordRow = document.querySelector(`.row-${row}`);
   const letterContainer = wordRow?.querySelector(`.letter-${guess}`);
@@ -24,6 +23,7 @@ document.addEventListener("keyup", (event) => {
   );
   let fullWord = "";
 
+  //handles letters
   if (
     letterContainer != undefined &&
     letter.test(keyClicked) === true &&
@@ -36,6 +36,7 @@ document.addEventListener("keyup", (event) => {
     guess++;
   }
 
+  //handles backspace
   if (keyClicked === "Backspace") {
     // need to delete previous letter
     if (guess === 0) {
@@ -48,16 +49,16 @@ document.addEventListener("keyup", (event) => {
     guess = guess - 1;
   }
 
+  // handles Enter: which checks if word is real
   if (keyClicked === "Enter") {
     for (let x = 0; x < 5; x++) {
       fullWord += wordRow?.querySelector(`.letter-${x}`)?.innerHTML;
     }
 
     if (words.includes(fullWord)) {
+      // in here right code to compare guessed word to wordOfTheDay
       row = row + 1;
       guess = 0;
     }
   }
-
-  console.log(guess);
 });
