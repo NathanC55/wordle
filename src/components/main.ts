8da5fbf2147148c5cd23298dfa27e8f58b9f0089
Nathan Cortez <nath.co03@icloud.com>
2023-12-28 15:16:02 -0500

    makes code easier to read,

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,20 +1,97 @@
 // clear board when generating a new word
 // keep board when reloading page
+// deal with bug, when winning game, it lets you effect the word if page is refreshed
+let wordOfTheDay = localStorage.getItem("word");
 let wins: number = Number(localStorage.getItem("wins"));
 let losses: number = Number(localStorage.getItem("losses"));
 let streak: number = Number(localStorage.getItem("streak"));
+let tries = 0;
+let keysClicked = 0;
+let row: number = Number(localStorage.getItem("row")) || 1;
 let guessedWords: string[] = JSON.parse(
   localStorage.getItem("guessedWords") || "[]"
 );
 
-function main(words: string[]) {
-  guessedWords?.forEach((word) => {});
+const letter = /^[a-zA-Z]+$/;
+const fullKeyBoard = document.querySelectorAll(".letter-button");
 
-  const generateNewWordButton = document.querySelector(".word-generator");
+const updateWinItem = () => {
+  localStorage.setItem("wins", JSON.stringify(wins));
+  localStorage.setItem("streak", JSON.stringify(streak));
+};
+const updateLossItem = () => {
+  localStorage.setItem("losses", JSON.stringify(wins));
+  localStorage.setItem("streak", JSON.stringify(streak));
+};
+const updateRowItem = () => {
+  localStorage.setItem("row", JSON.stringify(row));
+};
 
-  generateNewWordButton?.addEventListener("click", () => {
-    generateNewWord();
+function wordCheck(word: string, wordRow: Element | null, correctCount = 0) {
+  for (let x = 0; x < 5; x++) {
+    if (wordOfTheDay != null) {
+      const letterClass =
+        wordOfTheDay[x] === word[x]
+          ? "correct"
+          : wordOfTheDay.includes(word[x])
+          ? "incorrect-spot"
+          : "wrong";
+
+      const currentElement = wordRow?.querySelector(`.letter-${x}`);
+      const buttonElement = document.querySelector(
+        `.${word[x].toLocaleUpperCase()}`
+      );
+
+      if (currentElement) {
+        setTimeout(() => {
+          currentElement.classList.add(letterClass);
+          currentElement.classList.add(`flip`);
+        }, x * 400);
+        setTimeout(() => {
+          buttonElement?.classList.add(`${letterClass}`);
+        }, 6 * 400);
+      }
+      if (typeof correctCount !== undefined) {
+        correctCount += wordOfTheDay[x] === word[x] ? 1 : 0;
+        console.log(correctCount);
+      }
+    }
+  }
+  return correctCount;
+}
+
+function saveGuessedWord(word: string) {
+  if (guessedWords === undefined) {
+    guessedWords = [];
+  }
+
+  guessedWords.push(word);
+  localStorage.setItem("guessedWords", JSON.stringify(guessedWords));
+  console.log(guessedWords);
+}
+guessedWords.forEach((guessedWord, index) => {
+  const wordRow = document.querySelector(`.row-${index + 1}`);
+  for (let x = 0; x < 5; x++) {
+    const letterContainer = wordRow?.querySelector(`.letter-${x}`);
+    if (letterContainer) {
+      letterContainer.innerHTML = guessedWord[x];
+    }
+
+    wordCheck(guessedWord, wordRow);
+  }
+});
+
+fullKeyBoard.forEach((buttonElement) => {
+  buttonElement.addEventListener("click", (event) => {
+    const button: string = buttonElement.innerHTML;
+
+    document.dispatchEvent(new KeyboardEvent("keyup", { key: button }));
   });
+});
+
+function main(words: string[]) {
+  // keep in main function----------
+  const generateNewWordButton = document.querySelector(".word-generator");
 
   function generateNewWord() {
     const fiveLetterWords = words.filter((word) => word.length === 5);
@@ -22,29 +99,16 @@ function main(words: string[]) {
       fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
     localStorage.setItem("word", newWord);
     wordOfTheDay = newWord;
-    console.log(newWord);
   }
-
-  let wordOfTheDay = localStorage.getItem("word");
 
   if (!wordOfTheDay) {
     generateNewWord();
   }
 
-  let tries = 0;
-  let keysClicked = 0;
-  let row = 1;
-
-  const letter = /^[a-zA-Z]+$/;
-  const fullKeyBoard = document.querySelectorAll(".letter-button");
-
-  fullKeyBoard.forEach((buttonElement) => {
-    buttonElement.addEventListener("click", (event) => {
-      const button: string = buttonElement.innerHTML;
-
-      document.dispatchEvent(new KeyboardEvent("keyup", { key: button }));
-    });
+  generateNewWordButton?.addEventListener("click", () => {
+    generateNewWord();
   });
+  // end -------------------------
 
   document.addEventListener("keyup", function keyUpEvent(event) {
     const wordRow = document.querySelector(`.row-${row}`);
@@ -54,23 +118,22 @@ function main(words: string[]) {
       `.letter-${keysClicked - 1}`
     );
     let fullWord = "";
-    let correctCount = 0;
-    //handles letters
-    function handleLetterClicks(key: string) {
-      if (
-        letterContainer != undefined &&
-        letter.test(clickedKey) === true &&
-        clickedKey.length === 1
-      ) {
-        letterContainer.innerHTML = key;
-        if (keysClicked === 5) {
-          return;
-        }
-
-        keysClicked++;
+    const deactivateGame = () => {
+      document.removeEventListener("keyup", keyUpEvent);
+    };
+    //handles clicked letters
+    if (
+      letterContainer != undefined &&
+      letter.test(clickedKey) === true &&
+      clickedKey.length === 1
+    ) {
+      letterContainer.innerHTML = clickedKey;
+      if (keysClicked === 5) {
+        return;
       }
+
+      keysClicked++;
     }
-    handleLetterClicks(clickedKey);
 
     //handles backspace
     if (clickedKey === "backspace") {
@@ -84,75 +147,37 @@ function main(words: string[]) {
       keysClicked = keysClicked - 1;
     }
 
-    function wordCheck(word: string) {
-      for (let y = 0; y < 5; y++) {
-        if (wordOfTheDay != null) {
-          const letterClass =
-            wordOfTheDay[y] === word[y]
-              ? "correct"
-              : wordOfTheDay.includes(word[y])
-              ? "incorrect-spot"
-              : "wrong";
-
-          const currentElement = wordRow?.querySelector(`.letter-${y}`);
-          const buttonElement = document.querySelector(
-            `.${word[y].toLocaleUpperCase()}`
-          );
-
-          if (currentElement) {
-            setTimeout(() => {
-              currentElement.classList.add(letterClass);
-              currentElement.classList.add(`flip`);
-            }, y * 400);
-            setTimeout(() => {
-              buttonElement?.classList.add(`${letterClass}`);
-            }, 6 * 400);
-          }
-
-          correctCount += wordOfTheDay[y] === word[y] ? 1 : 0;
-        }
-      }
-    }
-
-    // handles Enter: which checks if word is real
+    // handles Enter
     if (clickedKey === "enter") {
       for (let x = 0; x < 5; x++) {
         fullWord += wordRow?.querySelector(`.letter-${x}`)?.innerHTML;
       }
 
       if (words.includes(fullWord)) {
-        // in here right code to compare guessed word to wordOfTheDay
-        wordCheck(fullWord);
+        saveGuessedWord(fullWord);
 
-        if (guessedWords === undefined) {
-          guessedWords = [];
-        }
-
-        guessedWords.push(fullWord);
-        localStorage.setItem("guessedWords", JSON.stringify(guessedWords));
-        console.log(guessedWords);
-
-        if (correctCount === 5) {
-          document.removeEventListener("keyup", keyUpEvent);
+        if (wordCheck(fullWord, wordRow) === 5) {
+          deactivateGame();
 
           console.log("you win");
           wins += 1;
           streak += 1;
-          localStorage.setItem("wins", JSON.stringify(wins));
-          localStorage.setItem("streak", JSON.stringify(streak));
+
+          updateWinItem();
           loadStatistics();
+
           return;
         }
 
         row = row + 1;
+        updateRowItem();
         tries += 1;
 
         if (tries === 6) {
-          document.removeEventListener("keyup", keyUpEvent);
+          deactivateGame();
           losses += 1;
-          localStorage.setItem("losses", JSON.stringify(losses));
           streak = 0;
-          localStorage.setItem("streak", JSON.stringify(streak));
+          updateLossItem();
           loadStatistics();
           console.log("you lose");
         }
